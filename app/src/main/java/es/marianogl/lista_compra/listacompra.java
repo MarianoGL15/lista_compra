d263bce0726bbcf353e1906dcd089ac6e713f9e8
MarianoGL15 <104677501+MarianoGL15@users.noreply.github.com>
2022-05-06 22:00:32 +0200

    no message

--- app/src/main/java/es/marianogl/lista_compra/listacompra.java
+++ app/src/main/java/es/marianogl/lista_compra/listacompra.java
@@ -2,17 +2,17 @@ package es.marianogl.lista_compra;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+private ListView mShoppingList;
+private EditText mItemEdit;
+private Button mAddButton;
+public class listacompra extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-
-
-
-
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.lista_compra);
     }
+
+
+
 }
