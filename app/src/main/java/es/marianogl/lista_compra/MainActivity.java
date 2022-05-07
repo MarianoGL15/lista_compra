b8aab456a38358ca91c1489d21af9092cf3c0cb6
DESKTOP-GR0UTI7\asir <juanperezmartinez825@gmail.com>
2022-05-07 16:17:59 +0200

    no message

--- app/src/main/java/es/marianogl/lista_compra/MainActivity.java
+++ app/src/main/java/es/marianogl/lista_compra/MainActivity.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-
-
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public static void main(String args[])
+    {
+        System.out.println("Hola Mundo");
     }
 }
